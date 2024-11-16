cbdb9e0d556f818e34aad5e4660532303bc3ab2c
gustavoromerocl <gandresrp@gmail.com>
2024-11-16 14:26:37 -0300

    feat: create auth guad

---
+++ src/store/session/session.selectors.ts
@@ -0,0 +1,11 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { SessionState } from './session.reducer';
+
+export const selectSessionState = createFeatureSelector<SessionState>('session');
+
+export const selectIsLoggedIn = createSelector(
+  selectSessionState,
+  (state) => state.isLoggedIn
+);
+
+export const selectUser = createSelector(selectSessionState, (state) => state.user);
