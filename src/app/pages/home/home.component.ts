ce2295792d9cc094d18faa7b016a0f16dedceff9
gustavoromerocl <gandresrp@gmail.com>
2024-11-17 15:24:20 -0300

    feat: add product on home view

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../../store/products/product.reducer';
+import { Store } from '@ngrx/store';
+import { selectAllProducts } from '../../store/products/product.selectors';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  products$: Observable<Product[]>;
 
+  constructor(private store: Store) {
+    this.products$ = this.store.select(selectAllProducts);
+  }
 }
