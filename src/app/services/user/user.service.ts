0f1cb2dd2d77719ee6d23480f34fb1282f29b4d2
gustavoromerocl <gandresrp@gmail.com>
2024-11-17 09:11:51 -0300

    feat: create user service

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
 
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+  role?: string;
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
+  private users: User[] = []; // Almacenamiento temporal en memoria
 
-  constructor() { }
+  constructor() {}
+
+  // Registrar un nuevo usuario
+  registerUser(user: User): void {
+    this.users.push(user);
+  }
+
+  // Devuelve el usuario sin la contraseña
+  findUser(email: string, password: string): Omit<User, 'password'> | null {
+    const user = this.users.find(
+      (u) => u.email === email && u.password === password
+    );
+    if (user) {
+      const { password, ...userWithoutPassword } = user;
+      return userWithoutPassword;
+    }
+    return null;
+  }
+
+  // Obtener todos los usuarios (solo para verificar)
+  getAllUsers(): User[] {
+    return this.users;
+  }
 }
